7c91c784b6f2ab5e900fc97527c95248f809e0b5
David Rodríguez <deivid.rodriguez@riseup.net>
2019-03-20 21:33:39 +0100

    Remove --no-hardlinks and see what happens

--- lib/bundler/source/git/git_proxy.rb
+++ lib/bundler/source/git/git_proxy.rb
@@ -100,7 +100,7 @@ module Bundler
             SharedHelpers.filesystem_access(path.dirname) do |p|
               FileUtils.mkdir_p(p)
             end
-            git_retry %(clone #{uri_escaped_with_configured_credentials} "#{path}" --bare --no-hardlinks --quiet)
+            git_retry %(clone #{uri_escaped_with_configured_credentials} "#{path}" --bare --quiet)
             return unless extra_ref
           end
 
